9446857a2c695da1a04adbec08d47128fae09599
asitarobo <asitarobo@gmail.com>
2018-12-09 14:23:10 +0000

    Done Progate RoR 9 users_and_Posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_24_021512) do
+ActiveRecord::Schema.define(version: 2018_12_03_123757) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
